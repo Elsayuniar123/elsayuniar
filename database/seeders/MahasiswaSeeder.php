4567da963bd28ea9cdd91b0ce0fbbac6425810e1
elsayuniar <elsayuniar92059@gmail.com>
2023-12-20 16:49:45 +0700

    Final Project

---
+++ database/seeders/MahasiswaSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class MahasiswaSeeder extends Seeder
+{
+    
+    public function run(): void
+    {
+        DB::table('mahasiswas')->insert([
+            'npm' => "101010",
+            'nama' => 'Mahasiswa10',
+            'alamat' => 'Padang',
+            'no_hp' => '08122222',
+        ]);
+    }
+}
